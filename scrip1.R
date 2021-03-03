adedee1d2f55a1ff9b7c3d573d4d8efd81d9ec76
santiagoeguren <santiago.eguren@uda.edu.ar>
2021-03-02 21:42:22 -0300

    segundo

--- scrip1.R
+++ scrip1.R
@@ -1,16 +1,16 @@
 
 
-library(usethis)
 
 x=c(1,12)
 
 
-  
+hist(x)
+
 
 #Correr en linux
   
-git config --global user.email "santiago.eguren@uda.edu.ar"
-git config --global user.name "santiagoeguren"
+#git config --global user.email "santiago.eguren@uda.edu.ar"
+#git config --global user.name "santiagoeguren"
   
 
 
